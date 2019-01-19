f4f839acf31db8935ed56a165908c338c08e7850
Bastian Blokland <bastianblokland@gmail.com>
2019-01-19 12:00:44 +0200

    Add base project stucture using typscript and rollup

---
+++ rollup.config.js
@@ -0,0 +1,24 @@
+﻿import resolve from 'rollup-plugin-node-resolve';
+
+import pkg from './package.json'
+
+export default {
+    input: 'build/app.js',
+    output: {
+        file: pkg.main,
+        format: 'umd'
+    },
+    plugins: [
+        resolve({
+            jsnext: true,
+            main: true,
+            module: true
+        })
+    ],
+    onwarn: function (warning, warn) {
+        // Ignore 'Circular Dependency' warning as its caused by d3 and apparently harmless
+        if (warning.code === 'CIRCULAR_DEPENDENCY')
+            return;
+        warn(warning);
+    }
+};
---
+++ src/app.ts
@@ -0,0 +1,5 @@
+﻿import * as d3 from 'd3';
+
+d3.select('body');
+
+console.log('hello, world');
