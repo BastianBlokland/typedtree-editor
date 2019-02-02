2beb2f21674a36141e1d92be94c6e4635245a004
Bastian Blokland <bastianblokland@gmail.com>
2019-02-02 16:38:11 +0200

    Added utility for replacing the value of a field.

---
+++ src/treemodifications.ts
@@ -0,0 +1,19 @@
+﻿import * as Utils from "./utils";
+import * as Tree from "./tree";
+
+export function fieldWithValue<T extends Tree.Field>(
+    field: T, element: Tree.FieldElementType<T>, offset: number = 0): T {
+
+    switch (field.kind) {
+        case "stringArray":
+        case "numberArray":
+        case "booleanArray":
+        case "nodeArray":
+            // Unfortunately the type system cannot follow what we are doing here so some casts are required.
+            const arrayField = <Tree.OnlyArrayField<T>>field;
+            const newValue = Utils.withNewElement(arrayField.value, offset, <Tree.FieldElementType<Tree.Field>>element);
+            return <T>{ ...field, value: newValue };
+        default:
+            return <T>{ ...field, value: element };
+    }
+}
---
+++ tests/treemodifications.test.ts
@@ -0,0 +1,50 @@
+﻿import * as Tree from "../src/tree";
+import * as TreeModifications from "../src/treemodifications";
+
+test("fieldWithValueString", () => {
+    const field: Tree.StringField = { kind: "string", name: "testName", value: "oldValue" };
+    expect(TreeModifications.fieldWithValue(field, "newValue"))
+        .toEqual({ kind: "string", name: "testName", value: "newValue" });
+});
+
+test("fieldWithValueNumber", () => {
+    const field: Tree.NumberField = { kind: "number", name: "testName", value: 1 };
+    expect(TreeModifications.fieldWithValue(field, 2))
+        .toEqual({ kind: "number", name: "testName", value: 2 });
+});
+
+test("fieldWithValueBoolean", () => {
+    const field: Tree.BooleanField = { kind: "boolean", name: "testName", value: false };
+    expect(TreeModifications.fieldWithValue(field, true))
+        .toEqual({ kind: "boolean", name: "testName", value: true });
+});
+
+test("fieldWithValueNode", () => {
+    const field: Tree.NodeField = { kind: "node", name: "testName", value: Tree.createNode("old") };
+    expect(TreeModifications.fieldWithValue(field, Tree.createNode("new")))
+        .toEqual({ kind: "node", name: "testName", value: Tree.createNode("new") });
+});
+
+test("fieldWithValueStringArray", () => {
+    const field: Tree.StringArrayField = { kind: "stringArray", name: "testName", value: ["oldValue1", "oldValue2"] };
+    expect(TreeModifications.fieldWithValue(field, "newValue", 1))
+        .toEqual({ kind: "stringArray", name: "testName", value: ["oldValue1", "newValue"] });
+});
+
+test("fieldWithValueNumberArray", () => {
+    const field: Tree.NumberArrayField = { kind: "numberArray", name: "testName", value: [1, 2, 3] };
+    expect(TreeModifications.fieldWithValue(field, 1337, 0))
+        .toEqual({ kind: "numberArray", name: "testName", value: [1337, 2, 3] });
+});
+
+test("fieldWithValueBooleanArray", () => {
+    const field: Tree.BooleanArrayField = { kind: "booleanArray", name: "testName", value: [true, false, true] };
+    expect(TreeModifications.fieldWithValue(field, true, 1))
+        .toEqual({ kind: "booleanArray", name: "testName", value: [true, true, true] });
+});
+
+test("fieldWithValueNodeArray", () => {
+    const field: Tree.NodeArrayField = { kind: "nodeArray", name: "testName", value: [Tree.createNode("old1"), Tree.createNode("old2")] };
+    expect(TreeModifications.fieldWithValue(field, Tree.createNode("new"), 0))
+        .toEqual({ kind: "nodeArray", name: "testName", value: [Tree.createNode("new"), Tree.createNode("old2")] });
+});
