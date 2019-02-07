1491d612f91a51a72f6dc12a4e8394d6742c3973
Bastian Blokland <bastianblokland@gmail.com>
2019-02-07 23:38:09 +0200

    Add reorder buttons to array field values.
    Ux needs some work 😅
    But for now its a decent way to re-order array elements.

--- src/treedisplay.ts
+++ src/treedisplay.ts
@@ -103,12 +103,26 @@ function createField(
         field: T,
         changed: fieldChangedCallback<T>): void {
 
+        const array = <ReadonlyArray<Tree.FieldElementType<T>>>field.value;
         for (let i = 0; i < field.value.length; i++) {
-            const element = field.value[i];
+            const element = array[i];
             const yOffset = i * nodeFieldHeight;
-            parent.addText("arrayFieldIndexPrefix", `[${i}]`, { x: nameWidth, y: centeredYOffset + yOffset });
-            createElementValue(element, 20, yOffset, newElement => {
-                changed(TreeModifications.fieldWithElement(field, <Tree.FieldElementType<T>>newElement, i));
+            const yPos = centeredYOffset + yOffset;
+
+            // Reorder buttons
+            parent.addGraphics("fieldValueButton", "arrayOrderUp", { x: nameWidth, y: yPos - 5 }, () => {
+                const newArray = Utils.withSwappedElements(array, i, (i == 0 ? array.length : i) - 1);
+                changed(TreeModifications.fieldWithValue(field, <Tree.FieldValueType<T>><unknown>newArray));
+            });
+            parent.addGraphics("fieldValueButton", "arrayOrderDown", { x: nameWidth, y: yPos + 5 }, () => {
+                const newArray = Utils.withSwappedElements(array, i, (i + 1) % array.length);
+                changed(TreeModifications.fieldWithValue(field, <Tree.FieldValueType<T>><unknown>newArray));
+            });
+
+            // Prefix and element value
+            parent.addText("arrayFieldIndexPrefix", `[${i}]`, { x: nameWidth + 7, y: yPos });
+            createElementValue(element, 30, yOffset, newElement => {
+                changed(TreeModifications.fieldWithElement(field, newElement, i));
             });
         }
     }
