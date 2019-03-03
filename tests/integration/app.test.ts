c52e5dce9c49d7b5f101176bdd8a0cc9b96350b9
Bastian Blokland <bastianblokland@gmail.com>
2019-03-03 18:29:36 +0200

    Create test for toggling toolbox visiblity

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -8,8 +8,22 @@ describe("app", () => {
     });
 
     it("should load", async () => {
-        await expect(await page.title()).toMatch("TypedTree");
+        expect(await page.title()).toBe("TypedTree");
         await saveScreenshot("load");
+    });
+
+    it("can toggle toolbox visibility", async () => {
+        await page.click("#toolbox-toggle");
+        expect(await getToolboxVisibility()).toEqual("hidden");
+        await saveScreenshot("toolbox-hidden");
+
+        await page.click("#toolbox-toggle");
+        expect(await getToolboxVisibility()).toEqual("visible");
+        await saveScreenshot("toolbox-visible");
+
+        function getToolboxVisibility(): Promise<string | null> {
+            return page.$eval("#toolbox", t => (t as HTMLElement).style.visibility);
+        }
     });
 });
 
