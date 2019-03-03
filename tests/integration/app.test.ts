f60de87aa50c5276166c96ae3f4c6cb415f0a2e1
Bastian Blokland <bastianblokland@gmail.com>
2019-03-03 20:06:36 +0200

    Add test for creating a new tree.

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -2,6 +2,11 @@
  * @file Jest puppeteer test for app.ts
  */
 
+import * as Tree from "../../src/tree";
+import * as TreeParser from "../../src/tree.parser";
+import * as TreeScheme from "../../src/treescheme";
+import * as TreeSchemeParser from "../../src/treescheme.parser";
+
 describe("app", () => {
     beforeEach(async () => {
         await page.goto("http://localhost:3000");
@@ -12,9 +17,13 @@ describe("app", () => {
         await saveScreenshot("load");
     });
 
-    it("links to github", async () => {
-        await page.click("#github-button");
-        expect(page.url()).toBe("https://github.com/BastianBlokland/typedtree-editor");
+    it("can create new tree", async () => {
+        await page.click("#newtree-button");
+        await saveScreenshot("newtree");
+
+        // Expect current tree to be first type of the root alias of the scheme
+        const scheme = await getCurrentScheme();
+        expect(await getCurrentTree()).toEqual(Tree.createNode(scheme.rootAlias.values[0]));
     });
 
     it("can toggle toolbox visibility", async () => {
@@ -30,7 +39,34 @@ describe("app", () => {
             return page.$eval("#toolbox", t => (t as HTMLElement).style.visibility);
         }
     });
+
+    it("links to github", async () => {
+        await page.click("#github-button");
+        expect(page.url()).toBe("https://github.com/BastianBlokland/typedtree-editor");
+    });
 });
+
+async function getCurrentScheme(): Promise<TreeScheme.IScheme> {
+    const schemeJson: string | undefined = await page.evaluate("getCurrentSchemeJson()");
+    if (schemeJson !== undefined) {
+        const parseResult = TreeSchemeParser.parseJson(schemeJson);
+        if (parseResult.kind === "success") {
+            return parseResult.value;
+        }
+    }
+    throw new Error("No valid scheme found");
+}
+
+async function getCurrentTree(): Promise<Tree.INode> {
+    const treeJson: string | undefined = await page.evaluate("getCurrentTreeJson()");
+    if (treeJson !== undefined) {
+        const parseResult = TreeParser.parseJson(treeJson);
+        if (parseResult.kind === "success") {
+            return parseResult.value;
+        }
+    }
+    throw new Error("No valid tree found");
+}
 
 async function saveScreenshot(title: string): Promise<void> {
     const screenshotDir = "./screenshots";
