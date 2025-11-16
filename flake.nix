2316316f18f5f5391842f590533429ac79497e42
Bastian Blokland <mail@bastian.tech>
2025-11-16 18:33:01 +0200

    nix: Add depenencies

--- flake.nix
+++ flake.nix
@@ -8,13 +8,20 @@
   outputs = { nixpkgs, ... }:
   let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; };
+    pkgs = import nixpkgs { inherit system; config.allowUnfree = true; };
   in
   {
     devShells.${system}.default = pkgs.mkShell {
       packages = [
         pkgs.gnumake
+        pkgs.nodejs_24
+        pkgs.lsof
+        pkgs.google-chrome
       ];
+
+      PUPPETEER_SKIP_DOWNLOAD = true;
+      PUPPETEER_SKIP_CHROMIUM_DOWNLOAD = true;
     };
+
   };
 }
